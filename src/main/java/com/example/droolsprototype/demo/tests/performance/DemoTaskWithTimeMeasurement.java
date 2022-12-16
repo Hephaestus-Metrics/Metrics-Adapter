81437f2e695702c49ca7f2de2192158a80b97eb7
Michał Szczurek <73036080+Ra77us@users.noreply.github.com>
2022-12-16 22:13:30 +0100

    Measuring utils

--- src/main/java/com/example/droolsprototype/demo/tests/performance/DemoTaskWithTimeMeasurement.java
+++ src/main/java/com/example/droolsprototype/demo/tests/performance/DemoTaskWithTimeMeasurement.java
@@ -1,14 +1,12 @@
 package com.example.droolsprototype.demo.tests.performance;
 
 import com.example.droolsprototype.demo.SimpleDemoTask;
-import com.example.droolsprototype.demo.tests.utils.CSVUtils;
-import com.example.droolsprototype.execution.ExecutionService;
+import com.example.droolsprototype.execution.TestExecutionService;
 import com.example.droolsprototype.services.PrometheusQueryService;
 import io.github.hephaestusmetrics.model.metrics.Metric;
 import io.github.hephaestusmetrics.model.queryresults.RawQueryResult;
 import io.github.hephaestusmetrics.serialization.Translator;
 import org.kie.api.runtime.StatelessKieSession;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -21,13 +19,12 @@ import java.util.stream.Stream;
 @Component
 public class DemoTaskWithTimeMeasurement extends SimpleDemoTask {
 
-    @Value("${test.save.path}")
-    private String savePath;
-
     private final Translator translator = new Translator();
+    private TestExecutionService executionService;
 
-    public DemoTaskWithTimeMeasurement(PrometheusQueryService queryService, StatelessKieSession kieSession, ExecutionService executionService) {
-        super(queryService, kieSession, executionService);
+    public DemoTaskWithTimeMeasurement(PrometheusQueryService queryService, StatelessKieSession kieSession, TestExecutionService executionService) {
+        super(queryService, kieSession, null);
+        this.executionService = executionService;
     }
 
     public void run() {
--- src/main/java/com/example/droolsprototype/execution/ExecutionService.java
+++ src/main/java/com/example/droolsprototype/execution/ExecutionService.java
@@ -1,17 +1,8 @@
 package com.example.droolsprototype.execution;
 
-import com.example.droolsprototype.demo.tests.utils.CSVUtils;
 import com.example.droolsprototype.services.KubernetesManagementService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Mock class simulating this service communicating with some executor, who makes changes in the system
@@ -20,50 +11,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RestController
 public class ExecutionService {
 
-    @Value("${test.save.path}")
-    private String savePath;
-
-    private KubernetesManagementService kubernetesManagementService;
+    private final KubernetesManagementService kubernetesManagementService;
 
     public ExecutionService(KubernetesManagementService kubernetesManagementService) {
         this.kubernetesManagementService = kubernetesManagementService;
     }
-
-    private long startTime;
-    private long postQueryTime;
-    private long postShuffleTime;
-    private long timeAfterParse;
-    private int metricsNum;
-
-    private String msg = "None for now";
-
-    @GetMapping("/")
-    public String debugMessageEndpoint() {
-        return msg;
-    }
     public void log(String message){
         System.out.println("Executor: " + message);
-    }
-
-    private AtomicInteger counter = new AtomicInteger(0);
-    public void execute(String message, String message2){
-        this.counter.addAndGet(-1);
-        if (this.counter.get() == 0) {
-            var time = System.currentTimeMillis();
-            CSVUtils.saveToCsv(savePath, List.of(metricsNum, postQueryTime - startTime, timeAfterParse - postQueryTime ,time - postShuffleTime));
-        }
-    }
-
-    public void insertTimes(int metricsNum, long startTime, long postQueryTime, long postShuffleTime, long timeAfterParse) {
-        if (this.counter.get() != 0) {
-            CSVUtils.saveToCsv(savePath, List.of("=======ERROR======="));
-        }
-        this.counter.set(10);
-        this.metricsNum = metricsNum;
-        this.startTime = startTime;
-        this.postQueryTime = postQueryTime;
-        this.postShuffleTime = postShuffleTime;
-        this.timeAfterParse = timeAfterParse;
     }
 
     public void deletePod(String namespace, String podName) {
